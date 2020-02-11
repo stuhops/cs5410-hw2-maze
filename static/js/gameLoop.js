2477a63c8cd9a5db6719135b4b6e80e09a98ec66
stuhops <stuart.hopkins@aggiemail.usu.edu>
2020-02-10 19:48:08 -0700

    Added class code to generate image and fixed bugs

--- static/js/gameLoop.js
+++ static/js/gameLoop.js
@@ -1,32 +1,4 @@
 
-// <<<<<<<<<<<<<<< Begin Game Loop >>>>>>>>>>>>>>>>>>>>>>
-
-let prevTime = performance.now();
-
-
-let processInput = elapsedTime => {
-}
-
-
-let update = elapsedTime => {
-}
-
-
-let render = elapsedTime => {
-}
-
-let gameLoop = time => {
-  let elapsedTime = time - prevTime;
-  prevTime = time;
-
-  processInput(elapsedTime);
-  update(elapsedTime);
-  render(elapsedTime);
-
-  requestAnimationFrame(gameLoop);
-}
-
-// <<<<<<<<<<<<<<<<< End Game Loop >>>>>>>>>>>>>>>>>>>>>>
 
 // <<<<<<<<<<<<<< Begin Maze Creation >>>>>>>>>>>>>>>>>>>
 
@@ -47,20 +19,8 @@ createBlankMaze = (row, col) => {
         used: 0,
         adj: [],
       });
-      
-      if(i === 0)
-        maze[i][j].edges.n = 1;
-      else if(i-1 === row)
-        maze[i][j].edges.s = 1;
-
-      if(j === 0)
-        maze[i][j].edges.w = 1;
-      else if(j-1 === col)
-        maze[i][j].edges.e = 1;
-        
     }
   }
-
   return maze;
 }
 
@@ -76,10 +36,7 @@ let primsMaze = (maze, startX, startY) => {
   if(startY)
     currY = startY;
   
-  let cnt = 0;  // TEMPORARY DELETE WHEN DEBUGGING IS DONE
   while(true) {
-    // cnt++;  // TEMPORARY DELETE WHEN DEBUGGING IS DONE
-    // if(cnt === 5) break;  // TEMPORARY DELETE WHEN DEBUGGING IS DONE
 
     for(let i = -1; i < 2; i++) {
       if(currX+i >= 0 && currX+i < maze.length) {
@@ -108,28 +65,46 @@ let primsMaze = (maze, startX, startY) => {
     let adj = adjList.splice(Math.random() * adjList.length, 1);
     currX = adj[0].x;
     currY = adj[0].y;
-    console.log(maze[currX][currY]);
-    if(maze[currX][currY].adj.length) 
-      maze[currX][currY].adj.splice(Math.random() * maze[currX][currY].adj.length, 1);
+    if(maze[currX][currY].adj.length) {
+      let tmpAdj = maze[currX][currY].adj.splice(Math.random() * maze[currX][currY].adj.length, 1)[0][0];
 
-    // Make a wall on every other thing in the list
-    while(maze[currX][currY].adj.length > 0) {
-      let tmpAdj = maze[currX][currY].adj.pop()[0];
-      if(currX < tmpAdj.x)
-        maze[currX][currY].edges.e = 1;
-      else if(currX > tmpAdj.x)
-        maze[currX][currY].edges.w = 1;
-      else if(currY < tmpAdj.y)
-        maze[currX][currY].edges.n = 1;
-      else if(currY > tmpAdj.y)
-        maze[currX][currY].edges.s = 1;
+      if(currX < tmpAdj.x) {
+        maze[currX][currY].edges.e = maze[tmpAdj.x][tmpAdj.y];
+        maze[tmpAdj.x][tmpAdj.y].edges.w = maze[currX][currY];
+      }
+      else if(currX > tmpAdj.x) {
+        maze[currX][currY].edges.w = maze[tmpAdj.x][tmpAdj.y];
+        maze[tmpAdj.x][tmpAdj.y].edges.e = maze[currX][currY];
+      }
+      else if(currY < tmpAdj.y) {
+        maze[currX][currY].edges.s = maze[tmpAdj.x][tmpAdj.y];
+        maze[tmpAdj.x][tmpAdj.y].edges.n = maze[currX][currY];
+      }
+      else if(currY > tmpAdj.y) {
+        maze[currX][currY].edges.n = maze[tmpAdj.x][tmpAdj.y];
+        maze[tmpAdj.x][tmpAdj.y].edges.s = maze[currX][currY];
+      }
       else
         console.log(`ERROR: No walls were defined at x: ${currX}, y: ${currY}`)
     }
+
+    // // Make a wall on every other thing in the list
+    // while(maze[currX][currY].adj.length > 0) {
+    //   let tmpAdj = maze[currX][currY].adj.pop()[0];
+    //   if(currX < tmpAdj.x)
+    //     maze[currX][currY].edges.e = maze[tmpAdj.x][tmpAdj.y];
+    //   else if(currX > tmpAdj.x)
+    //     maze[currX][currY].edges.w = maze[tmpAdj.x][tmpAdj.y];
+    //   else if(currY < tmpAdj.y)
+    //     maze[currX][currY].edges.n = maze[tmpAdj.x][tmpAdj.y];
+    //   else if(currY > tmpAdj.y)
+    //     maze[currX][currY].edges.s = maze[tmpAdj.x][tmpAdj.y];
+    //   else
+    //     console.log(`ERROR: No walls were defined at x: ${currX}, y: ${currY}`)
+    // }
     // delete maze[currX][currY].adj;
   }
 
-  console.log(adjList);
   console.log(maze);
   return maze;
 }
@@ -140,10 +115,208 @@ let createMaze = (row, col) => {
 
   maze = primsMaze(maze);
 
-
   return maze;
 }
 
+// <<<<<<<<<<<<<<< End Maze Creation >>>>>>>>>>>>>>>>>>>>
 
-maze = createMaze(3, 3);
-// gameLoop();
+
+let inputBuffer = {};
+let canvas = null;
+let context = null;
+
+const COORD_SIZE = 1024;
+
+let imgFloor = new Image();
+imgFloor.isReady = false;
+imgFloor.onload = function() {
+    this.isReady = true;
+};
+imgFloor.src = 'static/images/floor.png';
+
+let maze = createMaze(3, 3);
+// let maze = [];
+// for (let row = 0; row < 3; row++) {
+//     maze.push([]);
+//     for (let col = 0; col < 3; col++) {
+//         maze[row].push({
+//             x: col, y: row, edges: {
+//                 n: null,
+//                 s: null,
+//                 w: null,
+//                 e: null
+//             }
+//         });
+//     }
+// }
+
+// maze[0][0].edges.s = maze[1][0];
+
+// maze[0][1].edges.s = maze[1][1];
+// maze[0][1].edges.e = maze[0][2];
+
+// maze[0][2].edges.w = maze[0][1];
+// maze[0][2].edges.s = maze[1][2];
+
+// maze[1][0].edges.n = maze[0][0];
+// maze[1][0].edges.e = maze[1][1];
+// maze[1][0].edges.s = maze[2][0];
+
+// maze[1][1].edges.n = maze[0][1];
+// maze[1][1].edges.s = maze[2][1];
+// maze[1][1].edges.w = maze[1][0];
+
+// maze[1][2].edges.n = maze[0][2];
+
+// maze[2][0].edges.n = maze[1][0];
+
+// maze[2][1].edges.n = maze[1][1];
+// maze[2][1].edges.e = maze[2][2];
+
+// maze[2][2].edges.w = maze[2][1];
+
+// <<<<<<<<<<<<< Begin Initialization >>>>>>>>>>>>>>>>>>>
+
+function drawCell(cell) {
+
+    if (imgFloor.isReady) {
+        context.drawImage(imgFloor,
+        cell.x * (COORD_SIZE / 3), cell.y * (COORD_SIZE / 3),
+        COORD_SIZE / 3 + 0.5, COORD_SIZE / 3 + 0.5);
+    }
+
+    if (cell.edges.n === null) {
+        context.moveTo(cell.x * (COORD_SIZE / 3), cell.y * (COORD_SIZE / 3));
+        context.lineTo((cell.x + 1) * (COORD_SIZE / 3), cell.y * (COORD_SIZE / 3));
+        // context.stroke();
+    }
+
+    if (cell.edges.s === null) {
+        context.moveTo(cell.x * (COORD_SIZE / 3), (cell.y + 1) * (COORD_SIZE / 3));
+        context.lineTo((cell.x + 1) * (COORD_SIZE / 3), (cell.y + 1) * (COORD_SIZE / 3));
+        // context.stroke();
+    }
+
+    if (cell.edges.e === null) {
+        context.moveTo((cell.x + 1) * (COORD_SIZE / 3), cell.y * (COORD_SIZE / 3));
+        context.lineTo((cell.x + 1) * (COORD_SIZE / 3), (cell.y + 1) * (COORD_SIZE / 3));
+        //context.stroke();
+    }
+
+    if (cell.edges.w === null) {
+        context.moveTo(cell.x * (COORD_SIZE / 3), cell.y * (COORD_SIZE / 3));
+        context.lineTo(cell.x * (COORD_SIZE / 3), (cell.y + 1) * (COORD_SIZE / 3));
+        //context.stroke();
+    }
+
+    //
+    // Can do all the moveTo and lineTo commands and then render them all with a single .stroke() call.
+    context.stroke();
+}
+
+
+function renderCharacter(character) {
+    if (character.image.isReady) {
+        context.drawImage(character.image,
+        character.location.x * (COORD_SIZE / 3), character.location.y * (COORD_SIZE / 3));
+    }
+}
+
+
+function moveCharacter(key, character) {
+    if (key === 'ArrowDown') {
+        if (character.location.edges.s) {
+            character.location = character.location.edges.s;
+        }
+    }
+    if (key == 'ArrowUp') {
+        if (character.location.edges.n) {
+            character.location = character.location.edges.n;
+        }
+    }
+    if (key == 'ArrowRight') {
+        if (character.location.edges.e) {
+            character.location = character.location.edges.e;
+        }
+    }
+    if (key == 'ArrowLeft') {
+        if (character.location.edges.w) {
+            character.location = character.location.edges.w;
+        }
+    }
+}
+
+
+function renderMaze() {
+    context.strokeStyle = 'rgb(255, 255, 255)';
+    context.lineWidth = 6;
+
+    for (let row = 0; row < 3; row++) {
+        for (let col = 0; col < 3; col++) {
+            drawCell(maze[row][col]);
+        }
+    }
+
+    context.beginPath();
+    context.moveTo(0, 0);
+    context.lineTo(COORD_SIZE - 1, 0);
+    context.lineTo(COORD_SIZE - 1, COORD_SIZE - 1);
+    context.lineTo(0, COORD_SIZE - 1);
+    context.closePath();
+    context.strokeStyle = 'rgb(0, 0, 0)';
+    context.stroke();
+}
+
+
+//
+// Immediately invoked anonymous function
+//
+let myCharacter = function(imageSource, location) {
+    let image = new Image();
+    image.isReady = false;
+    image.onload = function() {
+        this.isReady = true;
+    };
+    image.src = imageSource;
+    return {
+        location: location,
+        image: image
+    };
+}('./static/images/character.png', maze[1][1]);
+
+
+function render() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    renderMaze();
+    renderCharacter(myCharacter);
+}
+
+
+function processInput() {
+    for (input in inputBuffer) {
+        moveCharacter(inputBuffer[input], myCharacter);
+    }
+    inputBuffer = {};
+}
+
+
+function gameLoop() {
+    processInput();
+    render();
+
+    requestAnimationFrame(gameLoop);
+
+}
+
+
+function initialize() {
+    canvas = document.getElementById('canvas-main');
+    context = canvas.getContext('2d');
+
+    window.addEventListener('keydown', function(event) {
+        inputBuffer[event.key] = event.key;
+    });
+
+    requestAnimationFrame(gameLoop);
+}
